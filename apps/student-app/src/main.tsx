c45f425e61d007f1a978f402b79ff0c0b8f0e3e0
33268 <332689063@qq.com>
2025-10-06 16:36:59 +0800

    fix: remove non-existent ThemeProvider import

--- apps/student-app/src/main.tsx
+++ apps/student-app/src/main.tsx
@@ -1,15 +1,12 @@
 ﻿import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { StudentProvider } from './store/studentStore';
-import { ThemeProvider } from '@kids/ui-kit';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <StudentProvider>
-        <App />
-      </StudentProvider>
-    </ThemeProvider>
+    <StudentProvider>
+      <App />
+    </StudentProvider>
   </StrictMode>,
 );
